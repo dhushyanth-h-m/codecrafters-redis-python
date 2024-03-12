e6dbcbc5fd7124c36f9e655b4c5f4c16048b42fa
Dhushyanth H M <dxh8160@mavs.uta.edu>
2024-03-12 01:14:56 -0500

    Handled multiple client requests using threading

--- app/main.py
+++ app/main.py
@@ -1,21 +1,25 @@
-# Uncomment this to pass the first stage
 import socket
+import threading
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-    # Uncomment this to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    c, addr = server_socket.accept() # wait for client
-    
-    while(True):
-        data = c.recv(1024)
-        if not data:
-            break
-        c.sendall(b"+PONG\r\n")
 
+    def handle_client(c):
+            while(True): # Keep the connection open and respond to PINGs with PONGs
+                data = c.recv(1024)
+                if not data:
+                    break
+                c.sendall(b"+PONG\r\n") 
+            c.close()
+    
+    while True:
+        client_socket, addr = server_socket.accept() # wait for client
+        print('Connection from', addr)
+        client_thread = threading.Thread(target=handle_client, args=(client_socket,))
+        client_thread.start()
+        
 if __name__ == "__main__":
     main()
